5f70cf428e503e52eb63814a24a78ae9735ee46c
kstgrd <kstgrd@gmail.com>
2025-07-07 13:09:52 -0500

    update

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -1,5 +1,5 @@
 yay -S --noconfirm --needed \
-  wget curl unzip inetutils \
+  wget curl unzip inetutils ly \
   fd eza fzf ripgrep zoxide bat \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
