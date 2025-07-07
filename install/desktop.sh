134a6226df5eb75305d0098cc37690be0d3db8d4
kstgrd <kstgrd@gmail.com>
2025-07-07 18:58:33 -0500

    update

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,6 +1,5 @@
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse-bin \
   nautilus sushi ffmpegthumbnailer gnome-calculator \
   chromium mpv \
